b11836dead4c937402d1069836f5aa8f347dd1a1
MrMoody098 <122175726+MrMoody098@users.noreply.github.com>
2023-02-11 00:02:18 +0000

    lava tower level generator(FIXED)

--- RedVBlue/Assets/Scripts/Obsticle.cs
+++ RedVBlue/Assets/Scripts/Obsticle.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.LowLevel;
 
 public class Obsticle : MonoBehaviour
 {
+    private rising rising;
+    public float maxDis = 100f;
     // Start is called before the first frame update
     void Start()
     {
-        
+     rising = FindObjectOfType<rising>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rising.transform.position.y >= gameObject.transform.position.y+50f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- RedVBlue/Assets/Scripts/levelGenerator.cs
+++ RedVBlue/Assets/Scripts/levelGenerator.cs
@@ -4,65 +4,65 @@ using UnityEngine;
 
 public class levelGenerator : MonoBehaviour
 {
-    public List<GameObject> prefabs;
+    public GameObject[] prefabs;
     public GameObject lavaPrefab;
     public float spawnY = 50f;
-    public float spawnInterval = 1f;
-    public int initialPrefabCount = 4;
+    public float waitTime = 0.5f;
 
-    private GameObject lastPrefab;
-    private GameObject lava;
+    private List<GameObject> generatedPrefabs;
+   // private GameObject lava;
+   // private Vector3 lavaStartPosition;
 
     void Start()
     {
+        generatedPrefabs = new List<GameObject>();
         StartCoroutine(GenerateInitialLevel());
     }
 
     IEnumerator GenerateInitialLevel()
     {
-        for (int i = 0; i < initialPrefabCount; i++)
+        Vector3 startPosition = transform.position;
+        for (int i = 0; i < 4; i++)
         {
-            Vector3 position = lastPrefab ? lastPrefab.transform.position + Vector3.up * spawnY : Vector3.zero;
-            int prefabIndex = Random.Range(0, prefabs.Count);
-            lastPrefab = Instantiate(prefabs[prefabIndex], position, Quaternion.identity);
+            int randomIndex = Random.Range(0, prefabs.Length);
+            GameObject prefab = Instantiate(prefabs[randomIndex], startPosition, Quaternion.identity);
+            generatedPrefabs.Add(prefab);
+            startPosition.y += spawnY;
         }
 
+      //  lavaStartPosition = startPosition - Vector3.up * spawnY;
+     //   lava = Instantiate(lavaPrefab, lavaStartPosition, Quaternion.identity);
+
+        yield return new WaitForSeconds(waitTime);
         StartCoroutine(GenerateLevel());
-        yield return null;
     }
 
     IEnumerator GenerateLevel()
     {
+        Vector3 startPosition = transform.position;
         while (true)
         {
-            Vector3 position = lastPrefab.transform.position + Vector3.up * spawnY;
-            int prefabIndex = Random.Range(0, prefabs.Count);
-            lastPrefab = Instantiate(prefabs[prefabIndex], position, Quaternion.identity);
-            StartCoroutine(DestroyPrefabWhenReachedByLava(lastPrefab));
-            yield return new WaitForSeconds(spawnInterval);
+            int randomIndex = Random.Range(0, prefabs.Length);
+            GameObject prefab = Instantiate(prefabs[randomIndex], startPosition, Quaternion.identity);
+            generatedPrefabs.Add(prefab);
+            startPosition.y += spawnY;
+            yield return new WaitForSeconds(waitTime);
         }
     }
 
-    IEnumerator DestroyPrefabWhenReachedByLava(GameObject prefab)
-    {
-        GameObject previousPrefab = lastPrefab;
-        if (!lava)
-        {
-            Vector3 lavaPosition = prefab.transform.position - Vector3.up * spawnY;
-            lava = Instantiate(lavaPrefab, lavaPosition, Quaternion.identity);
-        }
+    //IEnumerator DestroyPrefabWhenReachedByLava(GameObject prefab)
+    //{
+    //    while (lava.transform.position.y < prefab.transform.position.y)
+    //    {
+    //        yield return null;
+    //    }
 
-        while (lava.transform.position.y < prefab.transform.position.y)
-        {
-            yield return null;
-        }
-
-        if (lava.transform.position.y >= prefab.transform.position.y)
-        {
-            Destroy(previousPrefab);
-        }
-
-        lastPrefab = prefab;
-    }
-
+    //    if (lava.transform.position.y >= prefab.transform.position.y + 50f)
+    //    {
+    //        int prefabIndex = generatedPrefabs.IndexOf(prefab);
+    //        GameObject prefabToDestroy = generatedPrefabs[prefabIndex - 1];
+    //        generatedPrefabs.Remove(prefabToDestroy);
+    //        Destroy(prefabToDestroy);
+    //    }
+    //}
 }
