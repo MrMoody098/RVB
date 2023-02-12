555d8efe58cd71cb57d2c7202194d430252f6d32
Lorcan Turner <56739741+lopyxel@users.noreply.github.com>
2023-02-12 04:23:16 +0000

    rotating on wall collision

--- RedVBlue/Assets/Scripts/PlayerMovement.cs
+++ RedVBlue/Assets/Scripts/PlayerMovement.cs
@@ -109,13 +109,10 @@ public class PlayerMovement : MonoBehaviourPunCallbacks, IPunObservable
     }
     private void Movement()
     {
-       
-        if (Input.GetKeyDown("r")) 
-        { back = -transform.forward; rotating = true; }
-        if (rotating) { rotateMe(back); }
-
         if (isWalled)
         {
+            if(rotating)
+                rotateMe(back);
             if (Input.GetButton("Jump"))
             {
                 rb.velocity = camera.transform.forward * jumpForce;
@@ -207,10 +204,11 @@ public class PlayerMovement : MonoBehaviourPunCallbacks, IPunObservable
     }
     public Vector2 FindVelRelativeToLook()
     {
-        float lookAngle = body.transform.eulerAngles.y; //left and right
-        float moveAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg; //current angle between
+        float lookAngle = body.transform.eulerAngles.y;
 
-        float u = Mathf.DeltaAngle(lookAngle, moveAngle); //how far from look your looking
+        float moveAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg; 
+
+        float u = Mathf.DeltaAngle(lookAngle, moveAngle); //magnitude of l+m angles
         float v = 90 - u; //pivots X rotation so they rotate at the same speed
         //ie Y (left and right) and X rotate at the same speed and finish look ah the same time
        
@@ -230,21 +228,21 @@ public class PlayerMovement : MonoBehaviourPunCallbacks, IPunObservable
     }
     bool rotating = false;
     Vector3 back;
-    public void rotateMe(Vector3 to)
+    public void rotateMe(Vector3 to) //converts vector directions to quaternions
     {
         Vector3 direction = to - transform.forward;
         float d = Vector3.Dot(to, direction.normalized);
         if (d > 0.05)
-        {
+        {   
             transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, direction, 10 * Time.deltaTime, 0));
         }
-        else { rotating = false; }
+        else { rotating = false; back = Vector3.zero; }
     }
     private void OnCollisionEnter(Collision collision)
     {   //stick to wall if we are not on the ground and not grappling and hit into a wall
         if (!grounded && !grapplingGun.IsGrappling() 
             && collision.gameObject.layer == LayerMask.NameToLayer("grapplable"))
-        { wallTimer = wallTimerTime; rb.velocity = Vector3.zero; }
+        { wallTimer = wallTimerTime; rb.velocity = Vector3.zero; rotating = true; back = -transform.forward; }
 
         //falling off map ressets position
 
