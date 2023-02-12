885496e68f4ea94da7ca35769666b6b1eb1aa0eb
MrMoody098 <122175726+MrMoody098@users.noreply.github.com>
2023-02-12 20:51:38 +0000

    health pickup

---
+++ RedVBlue/Assets/healthP.cs
@@ -0,0 +1,22 @@
+using ExitGames.Client.Photon.StructWrapping;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthP : MonoBehaviour
+{
+    CharacterAttributes playerAtr;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided with powerup");
+        CharacterAttributes player = collision.gameObject.GetComponent<CharacterAttributes>();
+        if (player != null)
+        {
+            player.UpHealth(1);
+            Destroy(gameObject);
+        }
+    }
+}
