5d969ee4492ea7b481295877fafa86effd6209ec
MrMoody098 <122175726+MrMoody098@users.noreply.github.com>
2023-02-15 21:23:16 +0000

    dash power up, double jump power up added, fixed jumping + spawning in lava tower again
    dash power up triggered by F, one double jump from power up. jumping is working in correct as ground check looking from objects of layertype ground when most objects in  scenes are under grapplable, still need to configure a new layer to work for both

--- RedVBlue/Assets/Scripts/PlayerMovement.cs
+++ RedVBlue/Assets/Scripts/PlayerMovement.cs
@@ -20,7 +20,7 @@ public class PlayerMovement : MonoBehaviourPunCallbacks, IPunObservable
 
     private float xRotation, yRotation;
     [Range(0f, 200f)]
-    public float sensitivity = 50f; 
+    public float sensitivity = 50f;
 
     public float moveSpeed = 4500;
     public float maxSpeed = 20;
@@ -37,7 +37,7 @@ public class PlayerMovement : MonoBehaviourPunCallbacks, IPunObservable
     [Range(0, 50)]
     public float wallDashForce = 15;
     [Range(0, 10)]
-    public float wallTimerTime = 2;  
+    public float wallTimerTime = 2;
     private float wallTimer = 0;
 
     private bool isWalled = false;
@@ -45,9 +45,11 @@ public class PlayerMovement : MonoBehaviourPunCallbacks, IPunObservable
     public bool isAbleToMove = true;
     bool isJumping, isCrouching;
     public bool grounded;
-    
+    public bool canDoubleJump = true;
+    public int dashCounter = 0;
+
     private CapsuleCollider collider;
-    
+
     PhotonView view;
     RoomUI ui;
     void Awake()
@@ -58,7 +60,7 @@ public class PlayerMovement : MonoBehaviourPunCallbacks, IPunObservable
         camera = GetComponentInChildren<Camera>();
         grapplingGun = camera.GetComponentInChildren<GrapplingGun>();
         grapplingGun.GetComponent<firing>().player = transform;
-        
+
         view = GetComponent<PhotonView>();
         RoomUI.player = view;
         ui = FindObjectOfType<RoomUI>();
@@ -66,7 +68,7 @@ public class PlayerMovement : MonoBehaviourPunCallbacks, IPunObservable
     }
     private void FixedUpdate()
     {
-        if (!view.IsMine) 
+        if (!view.IsMine)
         { camera.GetComponent<Camera>().targetDisplay = 2; }
 
     }
@@ -79,13 +81,13 @@ public class PlayerMovement : MonoBehaviourPunCallbacks, IPunObservable
             MyInput();
             Movement();
             Look();
-            if (wallTimer > 0) 
-            { wallTimer -= Time.deltaTime; 
-                rb.useGravity = false; 
+            if (wallTimer > 0)
+            { wallTimer -= Time.deltaTime;
+                rb.useGravity = false;
                 isWalled = true; }
-            else if (!rb.useGravity) 
-            { rb.useGravity = true; 
-                rb.useGravity = true; 
+            else if (!rb.useGravity)
+            { rb.useGravity = true;
+                rb.useGravity = true;
                 isWalled = false; }
 
             checkIsGrounded();
@@ -103,7 +105,7 @@ public class PlayerMovement : MonoBehaviourPunCallbacks, IPunObservable
     {
         if (isWalled)
         {
-            if(ui.controls.quickRotate && rotating)
+            if (ui.controls.quickRotate && rotating)
                 rotateMe(back);
             if (Input.GetButton("Jump"))
             {
@@ -114,7 +116,7 @@ public class PlayerMovement : MonoBehaviourPunCallbacks, IPunObservable
             }
             return;
         }
-
+    
         if (isCrouching && grounded) { rb.drag = 0; }
         else { rb.drag = 1; }
         float vertical = Input.GetAxisRaw("Horizontal");
@@ -122,134 +124,171 @@ public class PlayerMovement : MonoBehaviourPunCallbacks, IPunObservable
         rb.velocity += vertical * body.right * moveSpeed * Time.deltaTime;
         rb.velocity += horizontal * body.forward * moveSpeed * Time.deltaTime;
 
-        if (!grounded) { return; }
-        if (Input.GetButton("Jump"))
-        { rb.velocity += Vector3.up * jumpForce/2; }
 
+        //if not grounded dont jump
+
+        //if double jump power enabled and not grounded can jump once in the air
+
+        //can jump when on the ground
+        if (grounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity += Vector3.up * jumpForce / 2;
+
+            }
+        }
+        //double jump
+        if (!grounded && canDoubleJump)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity += Vector3.up * jumpForce / 2;
+                canDoubleJump = false;
+            }
+        }
+        
         //Extra gravity
         rb.AddForce(Vector3.down * Time.deltaTime * 10);
 
-        //Find actual velocity relative to where player is looking
-        Vector2 mag = FindVelRelativeToLook();
-        float xMag = mag.x, yMag = mag.y;
+            //Find actual velocity relative to where player is looking
+            Vector2 mag = FindVelRelativeToLook();
+            float xMag = mag.x, yMag = mag.y;
 
-        ////Counteract sliding and sloppy movement
-        CounterMovement(vertical, horizontal, mag);
+            ////Counteract sliding and sloppy movement
+            CounterMovement(vertical, horizontal, mag);
 
-        //Set max speed
-        float maxSpeed = this.maxSpeed;
+            //Set max speed
+            float maxSpeed = this.maxSpeed;
 
-        //If sliding down a ramp, add force down so player stays grounded and also builds speed
-        if (isCrouching && grounded)
-        {rb.AddForce(Vector3.down * Time.deltaTime * 3000);
-            return;}
+            //If sliding down a ramp, add force down so player stays grounded and also builds speed
+            if (isCrouching && grounded)
+            { rb.AddForce(Vector3.down * Time.deltaTime * 3000);
+                return; }
 
-        ////If speed is larger than maxspeed, cancel out the input so you don't go over max speed
-        if (vertical > 0 && xMag > maxSpeed) vertical = 0;
-        if (vertical < 0 && xMag < -maxSpeed) vertical = 0;
-        if (horizontal > 0 && yMag > maxSpeed) horizontal = 0;
-        if (horizontal < 0 && yMag < -maxSpeed) horizontal = 0;
+            ////If speed is larger than maxspeed, cancel out the input so you don't go over max speed
+            if (vertical > 0 && xMag > maxSpeed) vertical = 0;
+            if (vertical < 0 && xMag < -maxSpeed) vertical = 0;
+            if (horizontal > 0 && yMag > maxSpeed) horizontal = 0;
+            if (horizontal < 0 && yMag < -maxSpeed) horizontal = 0;
 
-    }
-
-
-
-    private void Look()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.fixedDeltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.fixedDeltaTime;
-
-        //Find current look rotation
-        Vector3 rot = camera.transform.localRotation.eulerAngles;
-        yRotation = rot.y + mouseX;
-
-        //Rotate, and also make sure we dont over- or under-rotate.
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        //Perform the rotations
-        camera.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);
-        body.transform.localRotation = Quaternion.Euler(0, yRotation, 0);
-
-    }
-    private void CounterMovement(float x, float y, Vector2 mag)
-    {
-        if (!grounded || isJumping) return;
-
-        //Counter movement
-        if (Math.Abs(mag.x) > threshold && Math.Abs(x) < 0.05f || (mag.x < -threshold && x > 0) || (mag.x > threshold && x < 0))
+        ///DASHING POWERUP
+        if( dashCounter > 0) 
         {
-            rb.AddForce(moveSpeed * body.transform.right * Time.deltaTime * -mag.x * counterMovement);
+            if (Input.GetKeyDown(KeyCode.F))
+            { rb.velocity = camera.transform.forward * jumpForce;
+                dashCounter -= 1;
+            }
+                }
         }
-        if (Math.Abs(mag.y) > threshold && Math.Abs(y) < 0.05f || (mag.y < -threshold && y > 0) || (mag.y > threshold && y < 0))
+
+
+    public void dash() 
+    
+    {
+        dashCounter  = 5;
+    }
+        public void doubleJump()
         {
-            rb.AddForce(moveSpeed * body.transform.forward * Time.deltaTime * -mag.y * counterMovement);
-        }
 
-        //Limit diagonal running. This will also cause a full stop if sliding fast and un-crouching, so not optimal.
-        if (Mathf.Sqrt((Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.z, 2))) > maxSpeed)
+            canDoubleJump = true;
+        }
+        private void Look()
         {
-            float fallspeed = rb.velocity.y;
-            Vector3 n = rb.velocity.normalized * maxSpeed;
-            rb.velocity = new Vector3(n.x, fallspeed, n.z);
-        }
-    }
-    public Vector2 FindVelRelativeToLook()
-    {
-        float lookAngle = body.transform.eulerAngles.y;
+            float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.fixedDeltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.fixedDeltaTime;
 
-        float moveAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg; 
+            //Find current look rotation
+            Vector3 rot = camera.transform.localRotation.eulerAngles;
+            yRotation = rot.y + mouseX;
 
-        float u = Mathf.DeltaAngle(lookAngle, moveAngle); //magnitude of l+m angles
-        float v = 90 - u; //pivots X rotation so they rotate at the same speed
-        //ie Y (left and right) and X rotate at the same speed and finish look ah the same time
-       
-        float magnitue = rb.velocity.magnitude; //total over all speed
-        float yMag = magnitue * Mathf.Cos(u * Mathf.Deg2Rad); //total required resistance to fight against
-        float xMag = magnitue * Mathf.Cos(v * Mathf.Deg2Rad); //X and Y
+            //Rotate, and also make sure we dont over- or under-rotate.
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        return new Vector2(xMag, yMag); 
-    }
-    public void checkIsGrounded()
-    {
-        RaycastHit hit;
-        Ray ray = new Ray(transform.position, -transform.up);
-        if (Physics.Raycast(ray, out hit, 1f, ~LayerMask.NameToLayer("ground")))
-        { grounded = true; } else { grounded = false; }
-
-    }
-    bool rotating = false;
-    Vector3 back;
-    public void rotateMe(Vector3 to) //converts vector directions to quaternions
-    {
-        Vector3 direction = to - transform.forward;
-        float d = Vector3.Dot(to, direction.normalized);
-        if (d > 0.05)
-        {   
-            transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, direction, 10 * Time.deltaTime, 0));
-        }
-        else { rotating = false; back = Vector3.zero; }
-    }
-    private void OnCollisionEnter(Collision collision)
-    {   //stick to wall if we are not on the ground and not grappling and hit into a wall
-        if (!grounded && !grapplingGun.IsGrappling() 
-            && collision.gameObject.layer == LayerMask.NameToLayer("grapplable"))
-        { wallTimer = wallTimerTime; rb.velocity = Vector3.zero; rotating = true; back = -transform.forward; }
-
-        //falling off map ressets position
-
-        if (collision.gameObject.name == "fallOffPoint")
-        { gameObject.transform.position = GameObject.Find("spawn(1)").transform.position; 
-            characterAttributes.DownHealth(1); }
-
-    }
-    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting) { stream.SendNext(xRotation); stream.SendNext(yRotation); }
-        if (stream.IsReading) 
-        { xRotation = (float) stream.ReceiveNext(); yRotation = (float)stream.ReceiveNext();
+            //Perform the rotations
             camera.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);
-            body.localRotation = Quaternion.Euler(0, yRotation, 0);
+            body.transform.localRotation = Quaternion.Euler(0, yRotation, 0);
+
+        }
+        private void CounterMovement(float x, float y, Vector2 mag)
+        {
+            if (!grounded || isJumping) return;
+
+            //Counter movement
+            if (Math.Abs(mag.x) > threshold && Math.Abs(x) < 0.05f || (mag.x < -threshold && x > 0) || (mag.x > threshold && x < 0))
+            {
+                rb.AddForce(moveSpeed * body.transform.right * Time.deltaTime * -mag.x * counterMovement);
+            }
+            if (Math.Abs(mag.y) > threshold && Math.Abs(y) < 0.05f || (mag.y < -threshold && y > 0) || (mag.y > threshold && y < 0))
+            {
+                rb.AddForce(moveSpeed * body.transform.forward * Time.deltaTime * -mag.y * counterMovement);
+            }
+
+            //Limit diagonal running. This will also cause a full stop if sliding fast and un-crouching, so not optimal.
+            if (Mathf.Sqrt((Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.z, 2))) > maxSpeed)
+            {
+                float fallspeed = rb.velocity.y;
+                Vector3 n = rb.velocity.normalized * maxSpeed;
+                rb.velocity = new Vector3(n.x, fallspeed, n.z);
+            }
+        }
+        public Vector2 FindVelRelativeToLook()
+        {
+            float lookAngle = body.transform.eulerAngles.y;
+
+            float moveAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg;
+
+            float u = Mathf.DeltaAngle(lookAngle, moveAngle); //magnitude of l+m angles
+            float v = 90 - u; //pivots X rotation so they rotate at the same speed
+                              //ie Y (left and right) and X rotate at the same speed and finish look ah the same time
+
+            float magnitue = rb.velocity.magnitude; //total over all speed
+            float yMag = magnitue * Mathf.Cos(u * Mathf.Deg2Rad); //total required resistance to fight against
+            float xMag = magnitue * Mathf.Cos(v * Mathf.Deg2Rad); //X and Y
+
+            return new Vector2(xMag, yMag);
+        }
+        public void checkIsGrounded()
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(transform.position, -transform.up);
+            if (Physics.Raycast(ray, out hit, 1f, ~LayerMask.NameToLayer("ground")))
+            { grounded = true; } else { grounded = false; }
+
+        }
+        bool rotating = false;
+        Vector3 back;
+        public void rotateMe(Vector3 to) //converts vector directions to quaternions
+        {
+            Vector3 direction = to - transform.forward;
+            float d = Vector3.Dot(to, direction.normalized);
+            if (d > 0.05)
+            {
+                transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, direction, 10 * Time.deltaTime, 0));
+            }
+            else { rotating = false; back = Vector3.zero; }
+        }
+        private void OnCollisionEnter(Collision collision)
+        {   //stick to wall if we are not on the ground and not grappling and hit into a wall
+            if (!grounded && !grapplingGun.IsGrappling()
+                && collision.gameObject.layer == LayerMask.NameToLayer("grapplable"))
+            { wallTimer = wallTimerTime; rb.velocity = Vector3.zero; rotating = true; back = -transform.forward; }
+
+            //falling off map ressets position
+
+            if (collision.gameObject.name == "fallOffPoint")
+            { gameObject.transform.position = GameObject.Find("spawn(1)").transform.position;
+                characterAttributes.DownHealth(1); }
+
+        }
+        void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+            if (stream.IsWriting) { stream.SendNext(xRotation); stream.SendNext(yRotation); }
+            if (stream.IsReading)
+            { xRotation = (float)stream.ReceiveNext(); yRotation = (float)stream.ReceiveNext();
+                camera.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);
+                body.localRotation = Quaternion.Euler(0, yRotation, 0);
+            }
         }
     }
-}
